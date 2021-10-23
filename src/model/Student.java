0c52b497889bf9e8dd818c6c3a24b2ac31bfd3d5
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-10-23 13:22:40 +0530

    update model Student and StudentTM

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,107 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Student {
+    private String nic;
+    private String fullName;
+    private String address;
+    private LocalDate dateOfBirth;
+    private String contact;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String nic, String fullName, String address, LocalDate dateOfBirth, String contact, String email) {
+        this.nic = nic;
+        this.fullName = fullName;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.contact = contact;
+        this.email = email;
+    }
+
+    public static Student fromMap(String nic, Map<String, String> data) {
+        return new Student(
+                nic,
+                data.get("name"),
+                data.get("address"),
+                LocalDate.parse(data.get("dob")),
+                data.get("contact"),
+                data.get("email")
+        );
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Map<String, String> toMap() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", fullName);
+        map.put("address", address);
+        map.put("dob", dateOfBirth + "");
+        map.put("contact", contact);
+        map.put("email", email);
+        return map;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "nic='" + nic + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", address='" + address + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", contact='" + contact + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/model/StudentTM.java
+++ src/model/StudentTM.java
@@ -1,4 +1,50 @@
 package model;
 
 public class StudentTM {
+
+    private String nic;
+    private String fullName;
+    private String address;
+
+    public StudentTM() {
+    }
+
+    public StudentTM(String nic, String fullName, String address) {
+        this.nic = nic;
+        this.fullName = fullName;
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentTM{" +
+                "nic='" + nic + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
